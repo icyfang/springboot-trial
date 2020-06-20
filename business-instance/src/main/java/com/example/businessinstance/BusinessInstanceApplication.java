daaa115bda65eef33db29c61b2aa257619d737de
icyfang <chuche1993@qq.com>
2020-06-20 19:24:16 +0800

    add ReturnType

--- business-instance/src/main/java/com/example/businessinstance/BusinessInstanceApplication.java
+++ business-instance/src/main/java/com/example/businessinstance/BusinessInstanceApplication.java
@@ -12,6 +12,8 @@ public class BusinessInstanceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BusinessInstanceApplication.class, args);
+
     }
+
 
 }
--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,9 +1,26 @@
 package com.example.eurekaserver;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -14,4 +31,71 @@ public class EurekaServerApplication {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
+    @Qualifier("eurekaClient")
+    @Autowired
+    EurekaClient client;
+
+    @Autowired
+    EurekaServiceRegistry registry;
+
+    public EurekaClient getClient() {
+        return client;
+    }
+
+    @RequestMapping("/handle")
+    public List<String> handle() {
+        JsonMapper mapper = new JsonMapper();
+        return client.getApplications().getRegisteredApplications().stream().map(i -> {
+            try {
+                return mapper.writeValueAsString(i);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+    }
+
+
+}
+
+@ControllerAdvice("com.example.eurekaserver")
+class CustomeResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return ReturnType.of(body);
+    }
+}
+class ReturnType<T>{
+    String code;
+    T value;
+
+    public ReturnType(String code, T value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    static <T> ReturnType<T> of(T t) {
+        return new ReturnType<T>("200", t);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }

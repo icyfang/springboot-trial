6ffb21be6174bcb8e4685338679824c9bed895f5
icyfang <chuche1993@qq.com>
2020-07-03 16:07:13 +0800

    add mybatis-plus module

--- mybatis-plus/src/test/java/com/example/mybatisplus/MybatisPlusApplicationTests.java
+++ mybatis-plus/src/test/java/com/example/mybatisplus/MybatisPlusApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.businessinstance;
+package com.example.mybatisplus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BusinessInstanceApplicationTests {
+class MybatisPlusApplicationTests {
 
     @Test
     void contextLoads() {

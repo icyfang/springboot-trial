b929c6f75985c76e8a0116e2b03b444a6345af45
icyfang <chuche1993@qq.com>
2020-12-26 12:04:40 +0800

    enhance BatchMapper

--- mybatis-plus/src/main/java/com/example/mybatisplus/mapper/BatchMapper.java
+++ mybatis-plus/src/main/java/com/example/mybatisplus/mapper/BatchMapper.java
@@ -5,8 +5,14 @@ import com.example.mybatisplus.ApplicationContextHolder;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -15,8 +21,10 @@ import java.util.List;
  */
 public interface BatchMapper<T> {
 
+    Logger log = LoggerFactory.getLogger(BatchMapper.class);
+
     @Transactional(rollbackFor = Exception.class)
-    default void insertBatch(List<T> list) {
+    default boolean executeBatch(List<T> list, String methodName) {
 
         if (list.isEmpty()) {
             throw new RuntimeException();
@@ -27,8 +35,19 @@ public interface BatchMapper<T> {
 
             Class<?> mapperClass = this.getClass().getInterfaces()[0];
             BaseMapper mapper = (BaseMapper) session.getMapper(mapperClass);
+            Class<?> clazz = getGenericClass(mapperClass);
             for (int i = 0; i < list.size(); i++) {
-                mapper.insert(list.get(i));
+                Method method = mapper.getClass().getMethod(methodName, clazz);
+                if (method == null) {
+                    String s = "executeBatch failed. Please complete your custom method " + methodName + " in your mapper.xml";
+                    log.error(s);
+                    throw new RuntimeException(s);
+                }
+                try {
+                    method.invoke(mapper, list.get(i));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
                 if (i % 1000 == 999) {
                     session.commit();
                     session.clearCache();
@@ -36,6 +55,50 @@ public interface BatchMapper<T> {
             }
             session.commit();
             session.clearCache();
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    default Class<?> getGenericClass(Class<?> mapperClass) throws ClassNotFoundException {
+
+        Type[] genericInterfaces = mapperClass.getGenericInterfaces();
+        Class<?> clazz = null;
+        for (Type type : genericInterfaces) {
+            ParameterizedTypeImpl parameterizedType = (ParameterizedTypeImpl) type;
+            if (BatchMapper.class.equals(parameterizedType.getRawType())) {
+                clazz = Class.forName(parameterizedType.getActualTypeArguments()[0].getTypeName());
+            }
+        }
+        if (clazz == null) {
+            String message = mapperClass.getName() + " does no extends BatchMapper";
+            log.error(message);
+            throw new RuntimeException(message);
+        }
+        return clazz;
+    }
+
+    /**
+     * for PO with an auto_increment pk, use insertBatch
+     *
+     * @param list List of Object need to be persisted.
+     * @return
+     */
+    @Transactional(rollbackFor = Exception.class)
+    default boolean insert(List<T> list) {
+        try {
+            return executeBatch(list, "insertBatch");
+        } catch (Exception e) {
+            return executeBatch(list, "insert");
         }
     }
+
+    @Transactional(rollbackFor = Exception.class)
+    default boolean merge(List<T> list) {
+        return executeBatch(list, "merge");
+    }
+
+    void insertBatch(T ratePO);
 }

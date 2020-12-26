ab3c1ad951638bc8c8d61e4f94e4f300a3ef9296
icyfang <chuche1993@qq.com>
2020-12-26 12:11:15 +0800

    add method to generate merge clause

--- mybatis-plus/src/main/java/com/example/mybatisplus/codegen/CodeGenerator.java
+++ mybatis-plus/src/main/java/com/example/mybatisplus/codegen/CodeGenerator.java
@@ -1,5 +1,7 @@
 package com.example.mybatisplus.codegen;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
@@ -15,7 +17,10 @@ import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,7 +35,7 @@ public class CodeGenerator {
     public static String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
+        help.append("请输入").append(tip).append("：");
         System.out.println(help.toString());
         if (scanner.hasNext()) {
             String ipt = scanner.next();
@@ -42,6 +47,110 @@ public class CodeGenerator {
     }
 
     public static void main(String[] args) {
+        genPO();
+    }
+
+    public static void insertSql(Class<?> clazz) {
+
+        List<List<String>> lists = parsePO(clazz);
+        List<String> columns = lists.get(1);
+        List<String> params = lists.get(0);
+
+        StringBuilder builder = new StringBuilder();
+        TableName annotation = clazz.getAnnotation(TableName.class);
+        builder.append("<insert id =\"insert\" useGeneratedKeys=\"false\">\n");
+        builder.append("insert into ").append(annotation.value()).append("(");
+        for (int i = 0; i < columns.size(); i++) {
+            if (i == 0) {
+                builder.append(columns.get(i)).append("\n");
+            } else {
+                builder.append(",").append(columns.get(i)).append("\n");
+            }
+        }
+        builder.append(")\n").append("VALUES(\n");
+        for (int i = 0; i < params.size(); i++) {
+            if (i == 0) {
+                builder.append("#{").append(params.get(i)).append("}\n");
+            } else {
+
+                builder.append(",#{").append(params.get(i)).append("}\n");
+            }
+        }
+        builder.append(",'SYSTEM'\n,getdate()\n)\n").append("</insert>");
+        System.out.println(builder.toString());
+    }
+
+    public static void mergeSql(Class<?> clazz, List<String> pks) {
+
+        List<List<String>> lists = parsePO(clazz);
+        List<String> columns = lists.get(1);
+        List<String> params = lists.get(0);
+
+        StringBuilder builder = new StringBuilder();
+        StringBuilder update = new StringBuilder();
+        StringBuilder insert = new StringBuilder();
+        StringBuilder insertValue = new StringBuilder();
+        TableName annotation = clazz.getAnnotation(TableName.class);
+        builder.append("<insert id=\"merge\">\n");
+        builder.append("merge into ").append(annotation.value()).append(" a using \n(\n").append("select\n");
+        for (int i = 0; i < columns.size(); i++) {
+            String str = columns.get(i);
+            if (i == 0) {
+                builder.append("#{").append(params.get(i)).append("} as ").append(str).append("\n");
+            } else {
+                builder.append(",#{").append(params.get(i)).append("} as ").append(str).append("\n");
+            }
+            if (!pks.contains(str)) {
+                update.append("a.").append(str).append(" = b.").append(str).append(", ");
+            }
+            insert.append(str).append(", ");
+            insertValue.append("b.").append(str).append(", ");
+        }
+        update.replace(update.length() - 2, update.length(), "");
+        insert.replace(insert.length() - 2, insert.length(), ")");
+        insertValue.replace(insertValue.length() - 2, insertValue.length(), ")");
+        builder.append(") b\n").append("on (");
+        for (int i = 0; i < pks.size(); i++) {
+            if (i == 0) {
+                builder.append("a.").append(pks.get(i)).append(" = b.").append(pks.get(i)).append("\n");
+            } else {
+                builder.append("and a.").append(pks.get(i)).append(" = b.").append(pks.get(i)).append("\n");
+            }
+        }
+        builder.append(")\n").append("when matched\n").append("then\n").append("update set ").append(update)
+               .append("\n");
+        builder.append("when not matched\n").append("then\n").append("insert (").append(insert).append("\n");
+        builder.append("values (").append(insertValue);
+        builder.append("</insert>");
+        System.out.println(builder.toString());
+    }
+
+    private static List<List<String>> parsePO(Class<?> clazz) {
+
+        List<Field> fieldList = new ArrayList<>();
+        Class<?> superClass = clazz;
+        while (superClass != null) {
+            fieldList.addAll(Arrays.asList(superClass.getDeclaredFields()));
+            superClass = superClass.getSuperclass();
+        }
+        List<String> columns = new ArrayList<>();
+        List<String> params = new ArrayList<>();
+        for (Field item : fieldList) {
+
+            TableField column = item.getAnnotation(TableField.class);
+            if (column != null && !columns.contains(column.value())) {
+                columns.add(column.value());
+                if (item.getType() == (Date.class)) {
+                    params.add(item.getName() + ", jdbcType = TIMESTAMP");
+                } else {
+                    params.add(item.getName());
+                }
+            }
+        }
+        return Arrays.asList(params, columns);
+    }
+
+    private static void genPO() {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
 
@@ -146,3 +255,4 @@ public class CodeGenerator {
     }
 
 }
+

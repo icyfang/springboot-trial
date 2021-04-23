b1c4b18c8bd5e6a083cf9571522950945899d4f0
icyfang <chuche1993@qq.com>
2021-04-24 00:19:12 +0800

    add conversion from string to date

--- spring-context/src/main/java/com/example/springcontext/properties/BookController.java
+++ spring-context/src/main/java/com/example/springcontext/properties/BookController.java
@@ -2,9 +2,13 @@ package com.example.springcontext.properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/book")
@@ -17,6 +21,10 @@ public class BookController {
     @Autowired
     @Qualifier("bookProperty2")
     private BookProperty bookProperty2;
+
+    @Value("${book.editDate}")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    Date editDate;
 
     @GetMapping("/bookProperty")
     public BookProperty getBook() {
--- spring-context/src/main/java/com/example/springcontext/properties/BookPropertyConfig.java
+++ spring-context/src/main/java/com/example/springcontext/properties/BookPropertyConfig.java
@@ -3,6 +3,11 @@ package com.example.springcontext.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.support.FormattingConversionServiceFactoryBean;
+
+import java.util.Collections;
 
 @Configuration
 public class BookPropertyConfig {
@@ -11,5 +16,15 @@ public class BookPropertyConfig {
     @ConfigurationProperties("mark")
     BookProperty bookProperty2() {
         return new BookProperty();
+    }
+
+    @Bean
+    public ConversionService conversionService() {
+        FormattingConversionServiceFactoryBean factory = new FormattingConversionServiceFactoryBean();
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        factory.setFormatterRegistrars(Collections.singleton(registrar));
+        factory.afterPropertiesSet();
+        return factory.getObject();
     }
 }

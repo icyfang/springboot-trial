14cbc7cfec5b720152a6c210ee1a782f59607a6b
icyfang <chuche1993@qq.com>
2021-09-02 20:17:48 +0800

    add jetcache module

---
+++ jetcache/src/main/java/com/example/jetcache/JetcacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.jetcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetcacheApplication.class, args);
+    }
+
+}
---
+++ jetcache/src/test/java/com/example/jetcache/JetcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jetcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
